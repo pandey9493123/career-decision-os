0cf4aedb21f92d8e35aacc0324059c1d406ae218
Om Pandey <pandey9493@gmail.com>
2026-02-27 06:05:18 +0530

    Fixed Render port issue

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.get("/bookings", (req, res) => {
     res.json(bookings);
 });
 
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
